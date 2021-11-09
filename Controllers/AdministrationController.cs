682d18e7baaf500bb110fbf397dc4d5898de8156
Mosab Al Sherawy <mosabzx@hotmail.com>
2021-11-09 13:44:14 +0100

    Adding Roles

---
+++ Controllers/AdministrationController.cs
@@ -0,0 +1,62 @@
+﻿using IdentityCMS_Demo.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityCMS_Demo.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+
+        [HttpGet]
+        public IActionResult RoleList()
+        {
+            var role = roleManager.Roles;
+            return View(role);
+        }
+
+
+
+
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityRole identityRole = new IdentityRole { Name = model.RoleName };
+                IdentityResult result = await roleManager.CreateAsync(identityRole);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("RoleList", "Administration");
+                }
+                foreach (IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+            }
+            
+            return  View(model);
+        }
+
+        
+
+
+
+
+    }
+}
